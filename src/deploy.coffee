lucasmotta/dploy

commit ceede908f5bbeaf53fb729ee2e3d1ae32d9c46ca

Closes issue #22
The include parameter now works as expected and it will deploy to the
correct destination folder.

Take the following example:

```
path:
  local: release/
  remote: public_html/project_name/
include:
  "extra_files/**": "/"
```

The files matched by the `include` flag will be uploaded to this path
in your server: `public_html/project_name`.

Where before they would be uploaded to:
`public_html/project_name/extra_files`.

But I haven’t tested on Windows since I don’t have a machine to test
with me. So if anyone runs into any problems with the include flag, let
me know.

src/deploy.coffee
@@ -3,7 +3,7 @@ path 		= require "path"
 fs			= require "fs"
 YAML		= require "yamljs"
 Signal		= require "signals"
-expand		= require "glob-expand"
+glob		= require "glob"
 minimatch	= require "minimatch"
 prompt		= require "prompt"
 exec		= require("child_process").exec
@@ -71,7 +71,7 @@ module.exports = class Deploy
 
 	# Set the default config
 	setupDefaultConfig: ->
-		@config.scheme ?= "ftp" 
+		@config.scheme ?= "ftp"
 		@config.port ?= (if @config.scheme is "ftp" then 21 else 22)
 		@config.slots ?= 1
 		@config.revision ?= ".rev"
@@ -167,7 +167,7 @@ module.exports = class Deploy
 		console.log "Checking revisions...".bold.yellow
 		
 		# Retrieve the revision file from the server so we can compare to our local one
-		@connection.get @config.path.remote + @config.revision, (error, data) =>
+		@connection.get path.normalize(@config.path.remote + @config.revision), (error, data) =>
 			# If the file was not found, we need to create one with HEAD hash
 			if error
 				fs.writeFile @revisionPath, @local_hash, (error) =>
@@ -191,7 +191,7 @@ module.exports = class Deploy
 					@checkDiff @remote_hash, @local_hash
 
 
-	# Get the diff tree between the local and remote revisions 
+	# Get the diff tree between the local and remote revisions
 	checkDiff: (old_rev, new_rev) ->
 		console.log "Checking diffs...".bold.yellow
 
@@ -260,13 +260,18 @@ module.exports = class Deploy
 
 	# Include extra files from the config file
 	includeExtraFiles: ->
-		return false if @ignoreInclude
+		return no if @ignoreInclude
 
 		for key of @config.include
-			files = expand({ filter: "isFile", cwd:process.cwd() }, key.split(" "))
+			files = glob.sync(key)
+			# Match the path of the key object to remove everything that is not a glob
+			match = path.dirname(key).match(/^[0-9a-zA-Z_\-/\\]+/)
 			for file in files
-				@toUpload.push name:file, remote:@config.include[key] + file
-		return true
+				# If there's any match for this key, we remove from the remote file name
+				remoteFile = if match and match.length then file.substring match[0].length else file
+
+				@toUpload.push name:file, remote:@config.include[key] + remoteFile
+		yes
 
 
 	# Method to check if you can upload those files or not
@@ -301,7 +306,7 @@ module.exports = class Deploy
 		yes
 	
 	askBeforeUpload: ->
-		return unless @hasFilesToUpload() 
+		return unless @hasFilesToUpload()
 
 		scheme = properties:
 			answer:
@@ -312,11 +317,14 @@ module.exports = class Deploy
 
 		if @toDelete.length
 			console.log "Files that will be deleted:".bold.red
-			console.log("[ ? ]".grey, "#{file.name}".red) for file in @toDelete
+			for file in @toDelete
+				console.log("[ ? ]".grey, "#{file.remote}".red)
 
 		if @toUpload.length
 			console.log "Files that will be uploaded:".bold.blue
-			console.log("[ ? ]".blue, "#{file.name}".blue) for file in @toUpload
+			for file in @toUpload
+				remoteFile = path.normalize @config.path.remote + file.remote
+				console.log("[ ? ]".blue, "#{file.name}".blue, ">".green, "#{remoteFile}".blue)
 
 		prompt.message = "Question"
 
@@ -330,18 +338,19 @@ module.exports = class Deploy
 
 
 	startUploads: ->
-		return unless @hasFilesToUpload() 
+		return unless @hasFilesToUpload()
 
 		@upload @connection
 		i = @config.slots - 1
 		@setupMultipleServers() while i-- > 0
+		return
 
 	hasFilesToUpload: ->
 		if @toUpload.length == 0 and @toDelete.length == 0
 			console.log "No files to upload".blue
 			@removeConnections()
-			return false
-		return true
+			return no
+		yes
 
 	upload: (server) ->
 		# Files to delete
@@ -362,10 +371,10 @@ module.exports = class Deploy
 
 
 		for item in @toDelete
-			return console.log "todelete", item.name if !item.completed
+			return if !item.completed
 
 		for item in @toUpload
-			return console.log "toupload", item.name if !item.completed
+			return if !item.completed
 
 		# Everything is updated, we can finish the process now.
 		@removeConnections()
@@ -388,8 +397,8 @@ module.exports = class Deploy
 				@uploadItem server, item
 				return
 
-			# Create the folder recursively in the server 
-			server.mkdir @config.path.remote + folder, (error) =>
+			# Create the folder recursively in the server
+			server.mkdir path.normalize(@config.path.remote + folder), (error) =>
 				unless @dirCreated[folder]
 					if error
 						# console.log "[ + ]".green, "Fail creating directory: #{folder}:".red
@@ -412,7 +421,7 @@ module.exports = class Deploy
 	# Upload the file to the remote path
 	uploadItem: (server, item) =>
 		# Set the entire remote path
-		remote_path = @config.path.remote + item.remote
+		remote_path = path.normalize(@config.path.remote + item.remote)
 
 		# Upload the file to the server
 		server.upload item.name, remote_path, (error) =>
@@ -432,7 +441,7 @@ module.exports = class Deploy
 		item.started = true
 
 		# Set the entire remote path
-		remote_path = @config.path.remote + item.remote
+		remote_path = path.normalize(@config.path.remote + item.remote)
 
 		# Delete the file from the server
 		server.delete remote_path, (error) =>

src/dploy.coffee
@@ -23,7 +23,7 @@ module.exports = class DPLOY
 			# Check if we should ignore the include parameter for this deploy
 			@ignoreInclude = @servers.indexOf("-i") >= 0 or @servers.indexOf("--ignore-include") >= 0
 			# Remove the ignore flag from the server list
-			@servers = @servers.filter (value) -> value isnt "-i" and value isnt "--ignore-include" 
+			@servers = @servers.filter (value) -> value isnt "-i" and value isnt "--ignore-include"
 			# If you don't set any servers, add an empty one to upload the first environment only
 			@servers.push null if @servers.length is 0
 

src/generator.coffee
@@ -20,8 +20,9 @@ module.exports = class Generator
 	_generateConfig: =>
 		fileName = "dploy.yaml"
 
-		# If the file does not exist, copy the generator example to user's folder
-		fs.createReadStream(path.resolve(__dirname, "../generator/dploy.yaml")).pipe(fs.createWriteStream(fileName)) unless fs.existsSync fileName
+		unless fs.existsSync fileName
+			# If the file does not exist, copy the generator example to user's folder
+			fs.createReadStream(path.resolve(__dirname, "../generator/dploy.yaml")).pipe(fs.createWriteStream(fileName))
 
 		@_dployCompleted.dispatch()
 
@@ -48,6 +49,7 @@ module.exports = class Generator
 		fs.appendFile fileName, content, (error) =>
 			if error
 				console.log "Error:".bold.red, "The post-commit file could not be created. Check the permissions of the folder.".red
+				console.log "\t #{error}"
 				return @_postCommitCompleted.dispatch()
 
 			fs.chmodSync fileName, "0755"

commit 6f53e7a18c0fecac259944ce9afb6be3f738c4a8

Fix remote file paths on Windows
Deployment is broken on Windows, because of the changes introduced
in ceede908f5bbeaf53fb729ee2e3d1ae32d9c46ca. On Windows,
path.normalize() produces file URIs with backslash separators (i.e.
"path1\path2\file.fl"), whereas the FTP spec requires to use forward
slashes as directory separators.

  * Add _constructRemotePath method that replaces all occurrences of
  backslashes to forward slashes in a given string.
  * Call _constructRemotePath upon file upload and revision file check.

src/deploy.coffee
@@ -167,7 +167,7 @@ module.exports = class Deploy
 		console.log "Checking revisions...".bold.yellow
 		
 		# Retrieve the revision file from the server so we can compare to our local one
-		@connection.get path.normalize(@config.path.remote + @config.revision), (error, data) =>
+		@connection.get @_constructRemotePath(path.normalize(@config.path.remote + @config.revision)), (error, data) =>
 			# If the file was not found, we need to create one with HEAD hash
 			if error
 				fs.writeFile @revisionPath, @local_hash, (error) =>
@@ -424,7 +424,7 @@ module.exports = class Deploy
 	# Upload the file to the remote path
 	uploadItem: (server, item) =>
 		# Set the entire remote path
-		remote_path = path.normalize(@config.path.remote + item.remote)
+		remote_path = @_constructRemotePath(path.normalize(@config.path.remote + item.remote))
 
 		# Upload the file to the server
 		server.upload item.name, remote_path, (error) =>
@@ -484,6 +484,8 @@ module.exports = class Deploy
 			console.log "Upload completed for ".green + "#{@server}".bold.underline.green if displayMessage
 			@completed.dispatch()
 
+	# Change backslashes to forward slashes on Windows
+	_constructRemotePath: (str) -> str.replace /\\+/g, "/"
 
 	# Remove special chars
 	_removeSpecialChars: (str) -> str.replace /[\W]/g, ""